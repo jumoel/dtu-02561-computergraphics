3f7b0e6fabda2add889fb578d3346c55034232a0
Julian Møller <julian@julianmoeller.dk>
2009-12-16 15:30:29 +0100

    Exercise 2 done -- good enough at least

---
+++ exercise2/02561-02-00-2009.cpp
@@ -0,0 +1,96 @@
+/* 02561-02-00-2009.cpp*/
+
+#include <GL/glut.h>
+
+void initgl(void);
+
+void display (void);
+void axis (void);
+void reshape (int w, int h);
+
+int main (int argc, char** argv) {
+        
+        glutInit (&argc, argv);
+        glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+        glutInitWindowSize (500, 500);
+        glutInitWindowPosition (100, 100);
+        glutCreateWindow ("Exercise 02561-02");
+        initgl ();
+        glutDisplayFunc (display);
+        glutReshapeFunc (reshape);
+        glutMainLoop ();
+        return 0;
+}
+
+
+void initgl (void) {
+        glClearColor (0., 0., 0., 0.);
+        glShadeModel (GL_FLAT);
+        glMatrixMode (GL_PROJECTION);
+        glLoadIdentity ();
+        glOrtho (-6., 6., -6., 6., -6., 6.);
+        glMatrixMode (GL_MODELVIEW);
+		
+        glLoadIdentity ();
+}
+
+void reshape (int w, int h) {
+        glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+        glMatrixMode (GL_PROJECTION);
+        glLoadIdentity ();
+        glOrtho (-6., 6., -6., 6., -6., 6.);
+        glMatrixMode (GL_MODELVIEW);
+        glLoadIdentity ();
+}
+
+
+void display (void) {        
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glColor3f (1.,1.,1.);
+  glLoadIdentity ();
+
+  //gluLookAt(2, 2, 2, 0, 0, 0, 0, 0, 1);
+  glRotatef(35.26, 1.0, 0.0, 0.0);
+  glRotatef(-135.0, 0.0, 1.0, 0.0);
+  glRotatef(-90.0, 1.0, 0.0, 0.0);
+  
+  glutWireCube (1.);
+           
+  axis();
+  glFlush ();      
+}
+
+
+void axis (void) {
+        /*Plot part of axis and an auxiliary line*/
+        GLfloat v0[] = {0., 0., 0.};
+        GLfloat vx[]= {4., 0., 0.};
+        GLfloat vy[]= {0, 4., 0.};
+        GLfloat vz[]= {0., 0., 4.};
+
+        GLfloat v0x1[] = {1., 0., 0.};
+        GLfloat vyx1[] = {1., 3., 0.};
+        
+        glPushAttrib(GL_CURRENT_BIT);
+        glColor3f (1., 0., 0.);
+        glBegin (GL_LINES);
+                glVertex3fv (v0);
+                glVertex3fv (vx);
+        glEnd ();
+        
+        glBegin (GL_LINES);
+                glVertex3fv (v0);
+                glVertex3fv (vy);
+        glEnd ();
+        
+        glBegin (GL_LINES);
+                glVertex3fv (v0);
+                glVertex3fv (vz);
+        glEnd ();
+        
+        glBegin (GL_LINES);
+                glVertex3fv (v0x1);
+                glVertex3fv (vyx1);
+        glEnd ();
+        glPopAttrib();
+}

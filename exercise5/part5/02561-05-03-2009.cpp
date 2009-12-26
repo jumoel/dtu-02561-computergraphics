6ea01e08b81359e8f78d831e41e4350915f7a591
Julian Møller <julian@julianmoeller.dk>
2009-12-26 17:06:26 +0100

    Exercise 5 done

---
+++ exercise5/part5/02561-05-03-2009.cpp
@@ -0,0 +1,102 @@
+/*  02561-05-03.cpp
+ *
+ *  checker.c  = oglpg9.1
+ *  This program texture maps a checkerboard image onto
+ *  two rectangles.  This program clamps the texture, if
+ *  the texture coordinates fall outside 0.0 and 1.0.
+ */
+#include <GL/glut.h>
+
+/*	Create checkerboard texture	*/
+#define	checkImageWidth 64
+#define	checkImageHeight 64
+GLubyte checkImage[checkImageWidth][checkImageHeight][3];
+
+void makeCheckImage(void)
+{
+    int i, j, c;
+    
+    for (i = 0; i < checkImageWidth; i++) {
+	    for (j = 0; j < checkImageHeight; j++) {
+	        c = ((((i&0x8)==0)^((j&0x8))==0))*255;
+	        checkImage[i][j][0] = (GLubyte) c;
+	        checkImage[i][j][1] = (GLubyte) c;
+	        checkImage[i][j][2] = (GLubyte) c;
+	    }
+    }
+}
+
+void myinit(void)
+{    
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    makeCheckImage();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, checkImageWidth, 
+	                checkImageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 
+	                &checkImage[0][0][0]);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glEnable(GL_TEXTURE_2D);
+    glShadeModel(GL_FLAT);
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+        glTexCoord2f(0.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+
+        glTexCoord2f(2.0, 0.0);
+        glVertex3f(10.0, 0.0, 0.0);
+
+        glTexCoord2f(2.0, 1.2);
+        glVertex3f(10.0, 6.0, 0.0);
+
+        glTexCoord2f(1.0, 2.0);
+        glVertex3f(5.0, 10.0, 0.0);
+
+        glTexCoord2f(0.0, 1.2);
+        glVertex3f(0.0, 6.0, 0.0);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void myReshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    glOrtho (-2., 12., -2., 12., -6., 6.);
+
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+    glRotatef(45.0, 0.0, 0.0, 1.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0, 0.0, -3.6);
+}
+
+int
+main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("checker");
+    myinit();
+    glutReshapeFunc (myReshape);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;             /* ANSI C requires main to return int. */
+}

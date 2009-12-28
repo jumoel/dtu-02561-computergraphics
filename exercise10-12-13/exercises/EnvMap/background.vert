20f2108ebcd432ba7d7789ece534236e36e64c02
Julian Møller <julian@julianmoeller.dk>
2009-12-28 15:38:18 +0100

    10.2 done? :D

--- exercise10-12-13/exercises/EnvMap/background.vert
+++ exercise10-12-13/exercises/EnvMap/background.vert
@@ -1,4 +1,16 @@
+/*
 void main(void)
 {
 	gl_Position = ftransform();
+}
+*/
+varying vec3 R;
+
+void main()
+{
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    vec3 N = normalize(gl_NormalMatrix*gl_Normal);
+    vec4 eyePos = gl_ModelViewMatrix*gl_Vertex;
+    R = reflect(eyePos.xyz, N);
+    
 }
--- exercise10-12-13/exercises/EnvMap/envmap.cpp
+++ exercise10-12-13/exercises/EnvMap/envmap.cpp
@@ -20,170 +20,186 @@ static GLuint cubemap;
 static GLuint background;
 static GLuint glass;
 static GLuint normalmap;
+GLuint tex;
 
 void display() {
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 
-	// Set up modelview transformation
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	ball->do_spin();
-	ball->set_gl_modelview();
+  // Set up modelview transformation
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  ball->do_spin();
+  ball->set_gl_modelview();
 
-	//get camera position
-	Vec3f eye, at, up;
-	ball->get_view_param(eye, at, up);
+  //get camera position
+  Vec3f eye, at, up;
+  ball->get_view_param(eye, at, up);
 
-	//draw skybox
-	glUseProgram(background);
-	glutSolidCube(20.);
+  //draw skybox
+  glUseProgram(background);
 
-	//draw object
-	glUseProgram(glass);
+  GLuint texMapLocation;
+  texMapLocation = glGetUniformLocation(background, "texMap");
+  glUniform1i(texMapLocation,0);
 
-	//glutSolidTeapot(1.f);
-	//glutSolidSphere(1.f, 32, 32);
-	glutSolidTorus(0.5, 1., 32, 32);
+  
+  glBindTexture(GL_TEXTURE_CUBE_MAP, tex);
 
-	glUseProgram(0);
-	glutSwapBuffers();
+  glutSolidCube(20.);
 
-	check_gl_error();
+  //draw object
+  glUseProgram(glass);
+
+  //glutSolidTeapot(1.f);
+  //glutSolidSphere(1.f, 32, 32);
+  glutSolidTorus(0.5, 1., 32, 32);
+
+  glUseProgram(0);
+  glutSwapBuffers();
+
+  check_gl_error();
 }
 
 
 int old_state=GLUT_UP;
 void mouse(int button, int state, int x, int y)
 {
-	if (old_state==GLUT_UP && state==GLUT_DOWN)
-	{
-		if (button==GLUT_LEFT_BUTTON) 
-			ball->grab_ball(ROTATE_ACTION,Vec2i(x,y));
-		else if (button==GLUT_MIDDLE_BUTTON) 
-			ball->grab_ball(ZOOM_ACTION,Vec2i(x,y));
-		else if (button==GLUT_RIGHT_BUTTON) 
-			ball->grab_ball(PAN_ACTION,Vec2i(x,y));
-	}
-	if (old_state==GLUT_DOWN && state==GLUT_UP)
-		ball->release_ball();
-	old_state=state;
+  if (old_state==GLUT_UP && state==GLUT_DOWN)
+  {
+    if (button==GLUT_LEFT_BUTTON) 
+      ball->grab_ball(ROTATE_ACTION,Vec2i(x,y));
+    else if (button==GLUT_MIDDLE_BUTTON) 
+      ball->grab_ball(ZOOM_ACTION,Vec2i(x,y));
+    else if (button==GLUT_RIGHT_BUTTON) 
+      ball->grab_ball(PAN_ACTION,Vec2i(x,y));
+  }
+  if (old_state==GLUT_DOWN && state==GLUT_UP)
+    ball->release_ball();
+  old_state=state;
 }
 
 void motion(int x, int y)
 {
-	if (old_state==GLUT_DOWN)
-		ball->roll_ball(Vec2i(x,y));
+  if (old_state==GLUT_DOWN)
+    ball->roll_ball(Vec2i(x,y));
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-	switch(key)
-	{
-	case '\033': exit(0); break;
-	}
+  switch(key)
+  {
+  case '\033': exit(0); break;
+  }
 }
 
 void animate(int x)
 {
-	glutPostRedisplay();
-	glutTimerFunc(10, animate, 0);
+  glutPostRedisplay();
+  glutTimerFunc(10, animate, 0);
 }
 
 void reshape(int w, int h)
 {
-	width = w;
-	height = h;
+  width = w;
+  height = h;
 
-	glViewport(0, 0, width, height);	
+  glViewport(0, 0, width, height);	
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(65.f, float(width)/height, 0.1, 50.);
-	glMatrixMode(GL_MODELVIEW);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(65.f, float(width)/height, 0.1, 50.);
+  glMatrixMode(GL_MODELVIEW);
 
-	delete ball;
-	ball = new TrackBall(Vec3f(0), 5.f, 1.f, width, height);
+  delete ball;
+  ball = new TrackBall(Vec3f(0), 5.f, 1.f, width, height);
 }
 
 void init(void)
 {
-	glClearColor(1,0,0,0);
-	glEnable(GL_DEPTH_TEST);
+  glClearColor(1,0,0,0);
+  glEnable(GL_DEPTH_TEST);
 
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  
+  glGenTextures(1, &tex);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, tex);
 
-	//create cube map
-    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
-    glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
-    glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
-    glEnable(GL_TEXTURE_GEN_S);
-    glEnable(GL_TEXTURE_GEN_T);
-    glEnable(GL_TEXTURE_GEN_R);
-	glEnable(GL_TEXTURE_CUBE_MAP);
+  glTexParameterf(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameterf(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameterf(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_REPEAT);
 
-    static GLenum faceTarget[6] = {
-        GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-        GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-        GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-        GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-        GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-        GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
-    };
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    char *cube_fn[] = {
-        "textures/cm_left.ppm", 
-        "textures/cm_right.ppm", 
-        "textures/cm_top.ppm", 
-        "textures/cm_bottom.ppm", 
-        "textures/cm_back.ppm", 
-        "textures/cm_front.ppm" };
+  //create cube map
+  glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+  glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+  glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+  glEnable(GL_TEXTURE_GEN_S);
+  glEnable(GL_TEXTURE_GEN_T);
+  glEnable(GL_TEXTURE_GEN_R);
+  glEnable(GL_TEXTURE_CUBE_MAP);
 
-    int wi = 128, he = 128;
-    void *faces[6];
 
-    for (int i = 0; i < 6; i++) {
-        faces[i] = load_ppm(cube_fn[i], wi, he);
-    }
+  static GLenum faceTarget[6] = {
+    GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+    GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+    GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+    GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+    GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+    GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+  };
 
-    for (int i = 0; i < 6; i++) {
-        glTexImage2D(faceTarget[i], 0, GL_RGBA, 128, 128, 0, GL_RGBA, GL_UNSIGNED_BYTE, (GLubyte*)faces[i]);
-    }
+  char *cube_fn[] = {
+    "textures/cm_left.ppm", 
+    "textures/cm_right.ppm", 
+    "textures/cm_top.ppm", 
+    "textures/cm_bottom.ppm", 
+    "textures/cm_back.ppm", 
+    "textures/cm_front.ppm" };
 
-	//create normalmap
-	const char* normalmap_fn = "textures/normalmap.ppm";
+  int wi = 128, he = 128;
+  void *faces[6];
 
-	//TODO
+  for (int i = 0; i < 6; i++) {
+    faces[i] = load_ppm(cube_fn[i], wi, he);
+  }
 
-	//create shaders
-	background = create_program("background.vert", "background.frag");
-	glass = create_program("glass.vert", "glass.frag");
+  for (int i = 0; i < 6; i++) {
+    glTexImage2D(faceTarget[i], 0, GL_RGB, 128, 128, 0, GL_RGB, GL_BYTE, faces[i]);
+  }
 
-	check_gl_error();
+  //create normalmap
+  const char* normalmap_fn = "textures/normalmap.ppm";
+
+  //TODO
+
+  //create shaders
+  background = create_program("background.vert", "background.frag");
+  glass = create_program("glass.vert", "glass.frag");
+
+  check_gl_error();
 }
 
 int main(int argc, char** argv)
 {  
-	glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
-	glutInitWindowSize(768, 768);
-	glutInit(&argc, argv);
-	glutCreateWindow("02561-10: Environment Mapping and Bump Mapping");
-	glutDisplayFunc(display);
-	glutKeyboardFunc(keyboard);
-	glutTimerFunc(10, animate, 0);
-	glutMouseFunc(mouse);
-	glutMotionFunc(motion);
-	glutReshapeFunc(reshape);
+  glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
+  glutInitWindowSize(768, 768);
+  glutInit(&argc, argv);
+  glutCreateWindow("02561-10: Environment Mapping and Bump Mapping");
+  glutDisplayFunc(display);
+  glutKeyboardFunc(keyboard);
+  glutTimerFunc(10, animate, 0);
+  glutMouseFunc(mouse);
+  glutMotionFunc(motion);
+  glutReshapeFunc(reshape);
 
-	glewInit();
+  glewInit();
 
-	init();
+  init();
 
-	glutMainLoop();
-	return 0;
+  glutMainLoop();
+  return 0;
 }
 
 //awk 10/11/08
